9a27a67501d1ce7659f62ed7f28cbe297c9fdb1d
Yakup SENEL <yakup.senel33@gmail.com>
2025-03-24 11:46:16 +0100

    Challenge Utilisation of env variables

---
+++ index.js
@@ -0,0 +1,5 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env'})
+
+console.log(`I am ${process.env.MY_NAME} , wilder in ${process.env.MY_CITY} , and I love ${process.env.MY_LANGUAGE}`);
