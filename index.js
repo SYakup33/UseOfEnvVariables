f3e49cccf8e1fbd055d06f67f6e880a90476670c
yakup <yakup.senel33@gmail.com>
2025-03-24 17:18:45 +0100

    dotenv package removed

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '.env'})
-
 console.log(`I am ${process.env.MY_NAME} , wilder in ${process.env.MY_CITY} , and I love ${process.env.MY_LANGUAGE}`);
